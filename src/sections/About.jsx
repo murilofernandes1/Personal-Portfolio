006aca09999bdbfade77d4d2e3100e729973b105
Murilo <murigod8@gmail.com>
2025-07-14 20:53:26 -0300

    Updated About Me section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -4,7 +4,13 @@ const About = () => {
     <section id="about">
       <div className='container'>
       <h2 className="header">About Me</h2>
-      <p className="about">Hey! I’m Murilo, 19 years old, and I’ve been coding since 2023. From backend with C#, to frontend with React and JavaScript, and mobile apps with Expo and TypeScript—I’ve built solid skills across the stack. But what really drives me is creating apps that stand out. I’m not interested in building more of the same. I love experimenting, thinking outside the box, and bringing fresh ideas to life.</p>
+      <p className="about">
+      I'm Murilo, a Front-End Developer and a Game Development enthusiast in my free time.  
+      My main stack includes JavaScript, HTML, CSS, and React. I also have experience with C# from working on Unity projects.  
+      I'm currently looking for my first opportunity in the field and open to freelance projects as well.  
+      Feel free to contact me through the social media links above for collaborations or job opportunities.
+      </p>
+
       </div>
     </section>
   );
